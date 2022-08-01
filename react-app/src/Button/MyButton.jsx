ed391446a20ffb4671567c228e598c42bbba1668
Alexandr <a.lysenko00@gmail.com>
2022-08-01 17:09:41 +0300

    develop - feat - add MyButton component

---
+++ react-app/src/Button/MyButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+
+function Button(props) {
+    return (
+        <Button variant="contained">Hello World</Button>
+    );
+}
+
+export default Button;
